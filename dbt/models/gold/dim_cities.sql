73ea5b44b9cac44872f47e4170e35ae8c0a307f2
Kareem Adel <kareema9001@gmail.com>
2025-03-25 21:50:49 +0000

    Dimensions are created

---
+++ dbt/models/gold/dim_cities.sql
@@ -0,0 +1,20 @@
+WITH cities AS(
+    SELECT 
+        *
+    FROM 
+        {{ ref('stg_silver__cities') }}
+)
+SELECT 
+    {% if var('is_test_run', default=true) %}
+        top 10
+    {% endif %}  
+    ROW_NUMBER() OVER(ORDER BY city_name, iata_code) AS city_id,
+    city_name,
+    iata_code,
+    latitude,
+    longitude,
+    timezone,
+    country_iso2
+FROM 
+    cities 
+    
---
+++ dbt/models/gold/dim_countries.sql
@@ -0,0 +1,21 @@
+WITH countries AS(
+    SELECT 
+        *
+    FROM 
+        {{ ref('stg_silver__countries') }}
+)
+SELECT 
+    {% if var('is_test_run', default=true) %}
+        top 10
+    {% endif %}  
+    ROW_NUMBER() OVER(ORDER BY country_name) AS country_id,
+    country_name,
+    country_iso2,
+    capital,
+    continent,
+    currency_name,
+    population,
+    phone_prefix
+FROM 
+    countries 
+    
