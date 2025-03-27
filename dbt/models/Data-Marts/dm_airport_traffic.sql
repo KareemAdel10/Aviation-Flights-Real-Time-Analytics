b24c36b708b3dfb8b83b3363bfbb7df7252318cd
Kareem Adel <kareema9001@gmail.com>
2025-03-27 00:24:53 +0000

    docs + tests

--- dbt/models/Data-Marts/dm_airport_traffic.sql
+++ dbt/models/Data-Marts/dm_airport_traffic.sql
@@ -1,6 +1,6 @@
 WITH departures AS (
     SELECT 
-        departure_icao AS departure_airport_iata,
+        departure_icao AS departure_airport_icao,
         COUNT(flight_id) AS total_departures,
         AVG(departure_delay) AS avg_departure_delay
     FROM {{ ref('fact_flights') }}
@@ -9,7 +9,7 @@ WITH departures AS (
 
 arrivals AS (
     SELECT 
-        arrival_icao AS arrival_airport_iata,
+        arrival_icao AS arrival_airport_icao,
         COUNT(flight_id) AS total_arrivals,
         AVG(arrival_delay) AS avg_arrival_delay
     FROM {{ ref('fact_flights') }}
@@ -36,5 +36,5 @@ SELECT
     COALESCE(arr.total_arrivals, 0) AS total_arrivals,
     COALESCE(arr.avg_arrival_delay, 0) AS avg_arrival_delay
 FROM airport_details ad
- JOIN departures dep ON ad.icao_code = dep.departure_airport_iata
- JOIN arrivals arr ON ad.icao_code = arr.arrival_airport_iata
+ JOIN departures dep ON ad.icao_code = dep.departure_airport_icao
+ JOIN arrivals arr ON ad.icao_code = arr.arrival_airport_icao
